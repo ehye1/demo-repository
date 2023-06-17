acb74896e3f7d81c8b6057de647451dfe3c49b82
Lee Hye Won <thankyou@sookmyung.ac.kr>
2023-06-17 14:00:30 +0900

    백엔드 개발 11. @RequestMapping, @GetMapping, @RequestParam 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping("test")
 public class TestController {
@@ -11,7 +12,12 @@ public String testController(){
 return "Hello World!(안녕하세요!)"; 
 }
 @GetMapping("/{id}")
-public String testContollerWithPathVariables(@PathVariable(required =false) int id){
+public String testContollerWithPathVariables(@PathVariable(required =false) int
+id){
 return "Hello World! ID "+ id;
+}
+@GetMapping("/Param")
+public String testContollerRequestParam(@RequestParam(required =false) int id){
+return "Hello World! ID param "+ id;
 }
 }
